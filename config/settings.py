2a5a2b5808711b44ae41b73bec29bba2a44780ad
JW <mashi79513@gmail.com>
2018-11-12 21:45:59 +0900

    2nd

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')2eje=e&0kp9^wf*!n_8wo3y^0^8qa9mgr*wn99xh=b+=4l35@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
